105eb3ef0b5e3fd033eee8c6c71e4815517af078
Kakoti <Plabon.Kakoti@shell.com>
2017-09-25 10:40:15 +0200

    config-update

--- src/main/java/com/airport/mvc/config/AppConfig.java
+++ src/main/java/com/airport/mvc/config/AppConfig.java
@@ -2,14 +2,10 @@ package com.airport.mvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@Configuration
 @ComponentScan("com.airport.mvc")
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter {
@@ -20,10 +16,6 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		resolver.setPrefix("/WEB-INF/view/");
 		resolver.setSuffix(".jsp");
 		return resolver;
-	}
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
 	}
 
 }
